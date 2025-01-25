dc3ef144b88203a41d987bea78400f1e37f1639e
ricardochaves <ricardobchaves6@gmail.com>
2025-01-25 12:59:11 -0300

    chore: add more options to configure flask

--- app.py
+++ app.py
@@ -45,4 +45,7 @@ def hello_world():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000, debug=True)
+    SERVER_HOST = os.environ.get('SERVER_HOST', "0.0.0.0")
+    SERVER_PORT = int(os.environ.get('SERVER_PORT', 80))
+    DEBUG = os.environ.get('DEBUG', False)
+    app.run(host=SERVER_HOST, port=SERVER_PORT, debug=DEBUG)
